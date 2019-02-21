6ed0004e947a63f68d9b6b102b985e4800f5095c
Patrick <zhupx2003@126.com>
2019-02-21 11:45:09 +0800

    Initial Commit

---
+++ Alive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alive
+//
+//  Created by Rhapsody on 2019/2/21.
+//  Copyright © 2019 Back to Bedlam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
