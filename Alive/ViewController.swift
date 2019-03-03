e4cb5dfdb574e7dbf1185506a4e945dd4e018361
Patrick <zhupx2003@126.com>
2019-03-03 16:10:14 +0800

    edit the button

--- Alive/ViewController.swift
+++ Alive/ViewController.swift
@@ -15,8 +15,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    
     @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    @IBAction func secondTouchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0, green: 0.3027052879, blue: 0.6462088227, alpha: 0.7737951144)
+        }
     }
     
 
