0a5c658b957b7652f6feb80982d17b4ee209abd2
Patrick <zhupx2003@126.com>
2019-03-03 14:28:46 +0800

    add a button

--- Alive/ViewController.swift
+++ Alive/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+    }
+    
 
 }
 
